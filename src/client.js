45a3a254643796551ec2dca297e081798dc7df02
austinkelleher <austin.kell47@gmail.com>
2017-02-04 12:00:57 -0500

    Introduce fastclick.

--- src/client.js
+++ src/client.js
@@ -1,4 +1,7 @@
+var attachFastClick = require('fastclick');
 var numberSpinner = require('./components/number-spinner');
+
+attachFastClick(document.body);
 
 numberSpinner.renderSync({
     value: 5
